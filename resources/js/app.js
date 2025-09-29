c313db68e5fbf14356d2d3a935185f552605f078
ka-kng <ka.kengo0503@gmail.com>
2025-09-30 00:42:46 +0900

    営業日カレンダーの作成

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+document.addEventListener('DOMContentLoaded', function () {
+    let calendarEl = document.getElementById('calendar');
+
+    if (calendarEl) {
+        let calendar = new Calendar(calendarEl, {
+            plugins: [dayGridPlugin, interactionPlugin],
+            initialView: 'dayGridMonth',
+            locale: 'ja',
+        });
+        calendar.render();
+    }
+});
